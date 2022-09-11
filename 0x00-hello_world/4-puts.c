75a96e355fad8b4e1e35ec5fa119e484416a6959
Gosebo <kgoetegosebo@yahoo.com>
2022-09-11 01:51:02 -0700

    Changes on the 4z-puts.c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,16 +6,13 @@
  *
  * * main - print the string in the put function
  *
- * *
- *
  * * Description: using the main function
  *
  * * this program prints "Programming is like building a multilingual puzzle
  *
  * * Return: 0
  *
- * */
-
+ */
 int main(void)
 
 {

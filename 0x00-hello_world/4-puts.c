e9dfcc5a1c8a7b150a952be6c1c109c25de6c0ad
Gosebo <kgoetegosebo@yahoo.com>
2022-09-11 01:57:04 -0700

    Changes updated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,8 +6,6 @@
  *
  * * main - print the string in the put function
  *
- * * Description: using the main function
- *
  * * this program prints "Programming is like building a multilingual puzzle
  *
  * * Return: 0

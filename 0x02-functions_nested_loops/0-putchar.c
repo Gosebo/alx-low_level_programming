fcfe2aa0da6bfdf1c08aa2b34a5958ab90254c2f
Gosebo <kgoetegosebo@yahoo.com>
2022-09-13 07:57:57 -0700

    Changes updated

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,24 @@
 #include "main.h" 
-  
- /** 
-  * main - prints _putchar 
-  * 
-  * Return: 0 (Success) 
-  */ 
-  
- int main(void) 
- { 
-         _putchar('_'); 
-         _putchar('p'); 
-         _putchar('u'); 
-         _putchar('t'); 
-         _putchar('c'); 
-         _putchar('h'); 
-         _putchar('a'); 
-         _putchar('r'); 
-         _putchar('\n'); 
-  
-         return (0); 
- }
+
+
+/**
+ * main - Entry point
+ *
+ * Return: nothing, return void
+ */
+
+int main(void)
+{
+ _putchar('_');
+ _putchar('p');
+ _putchar('u');
+ _putchar('t');
+ _putchar('c');
+ _putchar('h');
+ _putchar('a');
+ _putchar('r');
+ _putchar('\n');
+ 
+ return (0);
+}
+

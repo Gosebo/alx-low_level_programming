ccaa3b75007b264c1e28ebc440d98df86203b3d6
Gosebo <kgoetegosebo@yahoo.com>
2022-10-04 13:07:34 -0700

    changes made

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,14 +1,38 @@
-#include "main.h"
-#include <stdio.h>
+#include "main.h"
+
+void print_number(int num);
+
 /**
- * main - Mian
- * @argc: int type
- * @argv: char type
- * Return: printis files name
- **/
-int main(int argc, char *argv[])
+
+* main - Prints the number of arguments passed into it
+ *
+ * @argc: Length of @argv
+ *
+* @argv: Array of strings of the programs arguments
+ *
+ * Return: 0, Success
+*/
+
+
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-(void) argv;
-printf("%d\n", argc - 1);
-return (0);
+print_number(--argc);
+_putchar('\n');
+return (0);
+}
+
+/**
+* print_number - prints number with _putchar
+*
+* @num: inter, number to be printed
+*
+* Return: void
+*/
+
+void print_number(int num)
+{
+if (num / 10)
+print_number(num / 10);
+
+_putchar(num % 10 + '0');
 }

1dd56055ff44ca68cb14965a888fb5969c459690
Gosebo <kgoetegosebo@yahoo.com>
2022-10-09 06:18:01 -0700

    Changes made

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,15 +1,16 @@
-#include "main.h" 
-/** 
- *_print_rev_recursion - Prints a string in reverse. 
- * Desc: _memset 
- * @s: char type 
- * Return: char type 
- **/ 
-void _print_rev_recursion(char *s) 
-{ 
-if (*s) 
-{ 
-_print_rev_recursion(s + 1); 
-_putchar(*s); 
-} 
+#include "main.h"
+
+/**
+ *_print_rev_recursion - Prints a string in reverse.
+ * Desc: _memset
+ * @s: char type
+ * Return: char type
+ **/
+void _print_rev_recursion(char *s)
+{
+if (*s)
+{
+_print_rev_recursion(s + 1);
+_putchar(*s);
+}
 }
--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,18 +1,20 @@
-#include "main.h" 
-/** 
- * factorial - Return the factorial of a given number. 
- * @n: int type 
- * Return: char type 
- **/ 
-int factorial(int n) 
-{ 
-if (n >= 1) 
-{ 
-return (n * factorial(n - 1)); 
-} 
-else if (n < 0) 
-return (-1); 
- 
-else 
-return (1); 
+#include "main.h"
+
+/**
+ * factorial - Return the factorial of a given number.
+ * @n: int type
+ * Return: char type
+ **/
+
+int factorial(int n)
+{
+if (n >= 1)
+{
+return (n * factorial(n - 1));
+}
+else if (n < 0)
+return (-1);
+
+else
+return (1);
 }
